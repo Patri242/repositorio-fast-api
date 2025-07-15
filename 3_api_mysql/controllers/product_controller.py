9206dfb5541f933b22f5daf131b7a908ab10db29
Patri <patricia.ruiz6248@gmail.com>
2025-07-15 19:01:59 +0200

    update

--- 3_api_mysql/controllers/product_controller.py
+++ 3_api_mysql/controllers/product_controller.py
@@ -35,6 +35,68 @@ async def obtener_por_id(id_product):
     
 
 #practicar. una ruta que me permita sacar uno o varios productos por precio min y precio max
+
+async def obtener_por_precio(min_price, max_price):
+    try:
+        conn=await get_connection() #abrimos conexion
+        async with conn.cursor(aiomysql.DictCursor) as cursor: #coloca el cursor abajo
+            await cursor.execute('SELECT * FROM upgrade_shop.products WHERE price between %s and %s',(min_price, max_price))
+            data= await cursor.fetchall()
+        conn.close()
+        if data:
+            return data
+        else:
+            raise HTTPException(status_code=404 , detail='Producto no encontrado')
+    except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Error de MYSQL: {str(e)}")
+
+
 #una ruta que me permita sacar un producto por su titulo. deberá devolverme listado de productos
+async def obtener_por_titulo(title):
+    try:
+        conn = await get_connection()
+        async with conn.cursor(aiomysql.DictCursor) as cursor:
+            query = 'SELECT * FROM upgrade_shop.products WHERE LOWER(title) LIKE %s'
+            value = f"%{title.lower()}%"
+            await cursor.execute(query, (value,))
+            data = await cursor.fetchall()
+        conn.close()
+        if data:
+            return data
+        else:
+            raise HTTPException(status_code=404, detail='Producto no encontrado')
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f'error mysql {str(e)}')
+    
+
 #una ruta que me permita devolver un listado de productos que no esten en stock.status:1
+async def obtener_por_stock():
+    try:
+        conn = await get_connection()
+        async with conn.cursor(aiomysql.DictCursor) as cursor:
+            await cursor.execute('SELECT * FROM upgrade_shop.products WHERE status= 0')
+            data = await cursor.fetchall()
+        conn.close()
+        if data:
+            return data
+        else:
+            raise HTTPException(status_code=404 , detail='Producto no encontrado')
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error de MYSQL: {str(e)}")
+    
 #una ruta que me permita devolver un listado de productos con cantidad mayor de 10
+
+async def obtener_por_cantidad():
+    try:
+        conn=await get_connection()
+        async with conn.cursor(aiomysql.DictCursor) as cursor:
+            await cursor.execute('SELECT * FROM upgrade_shop.products WHERE quantity > 10')
+            data = await cursor.fetchall()
+        conn.close()
+        if data:
+            return data
+        else:
+            raise HTTPException(status_code=404 , detail='Producto no encontrado')
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error de MYSQL: {str(e)}")
--- 3_api_mysql/routes/product_routes.py
+++ 3_api_mysql/routes/product_routes.py
@@ -10,6 +10,23 @@ async def get_all():
     return await product_controller.get_products_list()
 
 #realizar todo el flujo para obtener un producto por id
-@router.get('/{id_product}', status_code=200)
-async def get_by_id(id_product):
+@router.get('/id/{id_product}', status_code=200)
+async def get_by_id(id_product:int):
     return await product_controller.obtener_por_id(id_product)
+
+
+@router.get('/price/{min_price}/{max_price}', status_code=200)
+async def get_by_price(min_price:float, max_price:float):
+    return await product_controller.obtener_por_precio(min_price, max_price)
+
+@router.get('/title/{title}', status_code=200)
+async def get_by_title(title:str):
+    return await product_controller.obtener_por_titulo(title)
+
+@router.get('/stock/0', status_code=200)
+async def get_by_stock():
+    return await product_controller.obtener_por_stock()
+
+@router.get('/quantity', status_code=200)
+async def get_by_quantity():
+    return await product_controller.obtener_por_cantidad()
